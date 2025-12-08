34b94975852a25424517add412ed636e5351a146
Volodya <hohiop09@gmail.com>
2025-12-08 19:46:42 +0100

    fix: add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+const todos = [
+  { id: 1, title: 'Learn Angular', completed: false },
+  { id: 2, title: 'Build a Todo App', completed: true },
+  { id: 3, title: 'Master TypeScript', completed: false }
+]
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+  editing = false;
+  todos = todos;
 }
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,17 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { config } from './app/app.config.server';
+import { provideServerRendering } from '@angular/platform-server';
+import {
+  BootstrapContext,
+  bootstrapApplication,
+} from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+  bootstrapApplication(
+    AppComponent,
+    {
+      providers: [provideServerRendering()],
+    },
+    context,
+  ).catch((err: unknown) => console.error(err));
 
 export default bootstrap;
