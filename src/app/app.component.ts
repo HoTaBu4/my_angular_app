1c1dd392cc5bd1b1a9955602a49d59e82b613c13
Volodya <hohiop09@gmail.com>
2025-12-08 20:01:27 +0100

    fix: add two-way-biding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 const todos = [
   { id: 1, title: 'Learn Angular', completed: false },
   { id: 2, title: 'Build a Todo App', completed: true },
   { id: 3, title: 'Master TypeScript', completed: false }
 ]
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
