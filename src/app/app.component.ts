8f4b7a9f97cf7aad911cb6f3eb5a2b67673c0d9d
Volodya <hohiop09@gmail.com>
2025-12-10 12:52:52 +0100

    fix: add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 const todos = [
   { id: 1, title: 'Learn Angular', completed: false },
@@ -16,11 +16,48 @@ interface Todo {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   editing = false;
   todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required, 
+        Validators.minLength(3)
+      ],
+    })
+  })
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  get activeTodosCount(): number {
+    return this.todos.filter(todo => !todo.completed).length;
+  }
+
+  addTodo() {
+    if(this.title.invalid) {
+      return;
+    }
+
+    if(this.title.value.trim().length === 0) {
+      return;
+    }
+
+    const newToodo: Todo = {
+      id: this.todos.length + 1,
+      title: this.title.value,
+      completed: false
+    }
+
+    this.todos.push(newToodo);
+    this.title.reset();
+  }
 }
