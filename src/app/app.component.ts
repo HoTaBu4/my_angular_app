d069863602191a2f36e69e2d59dbb0cd153df367
Volodya <hohiop09@gmail.com>
2025-12-12 19:38:42 +0100

    feat: refactor todo component and implement editing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
+import { Todo } from './types/todo';
 
 const todos = [
   { id: 1, title: 'Learn Angular', completed: false },
@@ -8,21 +10,15 @@ const todos = [
   { id: 3, title: 'Master TypeScript', completed: false }
 ]
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule ,TodoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 
 export class AppComponent {
-  editing = false;
   todos = todos;
   todoForm = new FormGroup({
     title: new FormControl('', {
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,60 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-
-const todos = [
-  { id: 1, title: 'Learn Angular', completed: false },
-  { id: 2, title: 'Build a Todo App', completed: true },
-  { id: 3, title: 'Master TypeScript', completed: false },
-];
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../../types/todo';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
+  @Output()
+  delete = new EventEmitter();
+
+  @Input()
+  todo!: Todo;
+
+  @ViewChild('titleField') set titleField(value: ElementRef) {
+    if (value) {
+      value.nativeElement.focus()
+    }
+  };
+
   editing = false;
-  todos = todos;
-  todoForm = new FormGroup({
-    title: new FormControl('', {
-      nonNullable: true,
-      validators: [Validators.required, Validators.minLength(3)],
-    }),
-  });
-
-  get title() {
-    return this.todoForm.get('title') as FormControl;
-  }
-
-  get activeTodosCount(): number {
-    return this.todos.filter(todo => !todo.completed).length;
-  }
-
-  addTodo() {
-    if (this.title.invalid) {
-      return;
-    }
-
-    if (this.title.value.trim().length === 0) {
-      return;
-    }
-
-    const newTodo: Todo = {
-      id: this.todos.length + 1,
-      title: this.title.value,
-      completed: false,
-    };
-
-    this.todos.push(newTodo);
-    this.title.reset();
-  }
 }
